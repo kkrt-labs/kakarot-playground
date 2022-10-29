60a9164f3d3bbf96d08ec097ac6161c5685117b8
Elias Tazartes <elias.tazartes@gmail.com>
2022-10-30 00:17:55 +0100

    feat: add deploy and execute_at_address

--- context/cairoContext.tsx
+++ context/cairoContext.tsx
@@ -7,7 +7,7 @@ import { uint256ToBN } from 'starknet/dist/utils/uint256'
 import { IExecutionState } from 'types'
 
 export const starknetSequencerProvider = new Provider({
-  baseUrl: 'https://bdfc86a639604dd88613b31113d217fe.missena.io/',
+  baseUrl: 'http://54.194.247.225:5050',
 })
 
 type ContextProps = {
@@ -15,6 +15,15 @@ type ContextProps = {
   setAccountAddress: (address: string) => void
   executionState: IExecutionState
   startExecution: (byteCode: string, value: bigint, data: string) => void
+  deployEvmContract: (byteCode: string) => Promise<void> | undefined
+  executeAtAddress: (
+    evmContractAddress: string,
+    callData: string,
+    value: bigint,
+  ) => Promise<void> | undefined
+  contract: Contract | undefined
+  evmContractAddress: string
+  setEvmContractAddress: (newEvmContractAddress: string) => void
 }
 
 const initialExecutionState = {
@@ -28,7 +37,7 @@ const initialExecutionState = {
 }
 
 const KAKAROT_ADDRESS =
-  '0x05f2d38bc1909a188cf950e903a6e28db912dd11920369f64cb31540f050b1f2'
+  '0x00ee9d8104feedb4d853b0ec970ac5f4442343b5f0d6fc5818943f307ba50db9'
 
 export const CairoContext = React.createContext<ContextProps>({
   accountAddress: '',
@@ -36,6 +45,11 @@ export const CairoContext = React.createContext<ContextProps>({
   setAccountAddress: () => {},
   executionState: initialExecutionState,
   startExecution: () => undefined,
+  deployEvmContract: () => undefined,
+  executeAtAddress: () => undefined,
+  contract: undefined,
+  evmContractAddress: '',
+  setEvmContractAddress: () => undefined,
 })
 
 export const CairoProvider = ({ children }: PropsWithChildren<{}>) => {
@@ -49,18 +63,22 @@ export const CairoProvider = ({ children }: PropsWithChildren<{}>) => {
 
   const [contract, setContract] = useState<Contract>()
 
+  const [evmContractAddress, setEvmContractAddress] = useState<string>('')
+
   useEffect(() => {
-    starknetSequencerProvider
-      .getCode(KAKAROT_ADDRESS)
-      .then((response) =>
-        setContract(
-          new Contract(
-            response.abi,
-            contractAddress,
-            starknetSequencerProvider,
-          ),
-        ),
+    starknetSequencerProvider.getCode(KAKAROT_ADDRESS).then((response) => {
+      if (
+        response === undefined ||
+        !Array.isArray(response?.abi) ||
+        (Array.isArray(response.abi) && response.abi.length === 0)
+      ) {
+        return
+      }
+
+      setContract(
+        new Contract(response.abi, contractAddress, starknetSequencerProvider),
       )
+    })
   }, [contractAddress])
 
   const hex2bytes = (hexString: string) =>
@@ -93,6 +111,23 @@ export const CairoProvider = ({ children }: PropsWithChildren<{}>) => {
     )
   }
 
+  const deployEvmContract = async (byteCode: string) => {
+    await contract?.functions['deploy'](hex2bytes(byteCode))
+  }
+
+  const executeAtAddress = async (
+    _evmContractAddress: string,
+    callData: string,
+    value: bigint,
+  ) => {
+    console.log(value)
+    const response = await contract?.functions['execute_at_address'](
+      hex2bytes(_evmContractAddress),
+      hex2bytes(callData),
+    )
+    return response.transaction_hash
+  }
+
   return (
     <CairoContext.Provider
       value={{
@@ -100,6 +135,11 @@ export const CairoProvider = ({ children }: PropsWithChildren<{}>) => {
         setAccountAddress,
         executionState,
         startExecution,
+        deployEvmContract,
+        executeAtAddress,
+        contract,
+        evmContractAddress,
+        setEvmContractAddress,
       }}
     >
       {children}
